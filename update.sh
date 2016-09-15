d0e6502c2948780aa5a2dae3cfb37bcdc1c011e9
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-09-15 14:28:01 +0800

    update update script

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-curl -L "https://docs.google.com/spreadsheets/d/1Y16yA1nPqnan8e22MgMyxziaRgmjOU0sG1L0vH5XJ80/export?gid=0&format=csv" > vocabulary.csv
+curl -L "https://docs.google.com/spreadsheets/d/1Y16yA1nPqnan8e22MgMyxziaRgmjOU0sG1L0vH5XJ80/export?gid=0&format=csv" | tr -d "\r" | cat > vocabulary.csv
